26fa462f273aaa0a4ef9b23fa6e40e8490081425
romanovULSTU <i.romnow11@gmail.com>
2024-09-14 18:26:34 +0300

    озможности fetch

--- parallel_execution.js
+++ parallel_execution.js
@@ -1,22 +1,50 @@
-"use  strict";
+'use  strict';
 
-async function getAllProducts() {
-  const response = await fetch("https://dummyjson.com/products");
-  return response.json();
-}
+// async function getAllProducts() {
+//   const response = await fetch('https://dummyjson.com/products');
+//   return response.json();
+// }
 
-async function getProduct(id) {
-  const response = await fetch("https://dummyjson.com/products/" + id);
-  return response.json();
-}
+// async function getProduct(id) {
+//   const response = await fetch('https://dummyjson.com/products/' + id);
+//   return response.json();
+// }
+
+// async function getProductError(id) {
+//   const response = await fetch('https://dummyjsons.com/products/' + id);
+//   return response.json();
+// }
+
+// async function main() {
+//   // const res1 = await Promise.all([getProduct(1), getProduct(2)]);
+//   // console.log(res1);
+
+//   // const res2 = await Promise.allSettled([getProduct(1), getProductError(2)]);
+//   // console.log(res2);
+
+//   const res3 = await Promise.any([
+//     getProduct(1),
+//     getProduct(2),
+//     getProductError(3),
+//   ]);
+//   console.log(res3);
+// }
+
+// main();
 
 async function main() {
-  // ПОСЛЕДОВАТЕЛЬНЫЕ ЗАПРОСЫ
-  const { products } = await getAllProducts();
-  const res = await Promise.all(
-    products.map((product) => getProduct(product.id))
-  );
-  console.log(res);
+  const res = await fetch('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: 'michaelw',
+      password: 'michaelwpass',
+    }),
+  });
+  const data = await res.json();
+  console.log(data);
 }
 
 main();
